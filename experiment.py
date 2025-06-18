0f8d49e1d817be96a769586987e984cbf113c4be
Loyst <loui.coat@gmail.com>
2025-06-18 10:34:01 +0200

    Added experiment class to improve experiment generation.

---
+++ experiment.py
@@ -0,0 +1,48 @@
+import os
+import json
+from datetime import datetime
+from abc import ABC, abstractmethod
+
+
+class Experiment(ABC):
+    def __init__(
+        self,
+        name,
+        base_dir="./experiments",
+        n_questions_per_distance=10,
+        n_comments=0,
+        n_loops=0,
+        n_if=0
+    ):
+        self.name = name
+        self.base_dir = base_dir
+        self.experiment_path = os.path.join(self.base_dir, self.name)
+        self.timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        
+        self.n_questions_per_distance = n_questions_per_distance
+        self.n_comments = n_comments
+        self.n_loops = n_loops
+        self.n_if = n_if
+
+
+    def _setup_experiment_dir(self):
+        os.makedirs(self.experiment_path, exist_ok=True)
+        self._save_metadata()
+
+    def _save_metadata(self):
+        metadata = self._metadata_dict()
+        metadata["name"] = self.name
+        metadata["timestamp"] = self.timestamp
+        metadata_path = os.path.join(self.experiment_path, "metadata.json")
+        with open(file=metadata_path, mode="w") as f:
+            json.dump(metadata, f, indent=4)
+
+    @abstractmethod
+    def _metadata_dict(self):
+        """À surcharger pour ajouter des champs spécifiques à chaque type d’expérience"""
+        return {}
+
+    @abstractmethod
+    def generate(self):
+        """À surcharger pour générer les programmes java"""
+        pass
